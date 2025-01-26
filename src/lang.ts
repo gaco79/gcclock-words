71b2a6d9cd5a4813630caae112e5eabebf9e2bb4
Gareth <gareth@garethcooper.com>
2025-01-26 14:54:11 +0000

    Tentative de and ru translations

--- src/lang.ts
+++ src/lang.ts
@@ -17,7 +17,7 @@
   next_h_from_minute: 31 to reference the next hour (5) after 31 minutes past the current hour.
  */
 export const LINE_DEFS = {
-  "en-GB": [
+  'en-GB': [
     {
       "it's": { h: true },
       quarter: { m: [15, 45] },
@@ -88,6 +88,80 @@ export const LINE_DEFS = {
       elf: { h: 11, next_h_from_minute: 16 },
       twaalf: { h: 0, next_h_from_minute: 16 },
       uur: { m: [0] },
+    },
+  ],
+  de: [
+    {
+      'es ist': { h: true },
+      viertel: { m: [15, 45] },
+      halb: { m: [30] },
+    },
+    {
+      fünf: { m: [5, 25, 35, 55] },
+      zehn: { m: [10, 50] },
+      zwanzig: { m: [20, 40] },
+    },
+    {
+      vor: { m: [45, 50, 55] },
+      nach: { m: [5, 10, 15, 20, 25] },
+      ein: { h: 1, next_h_from_minute: 16 },
+      zwei: { h: 2, next_h_from_minute: 16 },
+    },
+    {
+      drei: { h: 3, next_h_from_minute: 16 },
+      vier: { h: 4, next_h_from_minute: 16 },
+      fünf: { h: 5, next_h_from_minute: 16 },
+    },
+    {
+      sechs: { h: 6, next_h_from_minute: 16 },
+      sieben: { h: 7, next_h_from_minute: 16 },
+      acht: { h: 8, next_h_from_minute: 16 },
+    },
+    {
+      neun: { h: 9, next_h_from_minute: 16 },
+      zehn: { h: 10, next_h_from_minute: 16 },
+      elf: { h: 11, next_h_from_minute: 16 },
+    },
+    {
+      zwölf: { h: 0, next_h_from_minute: 16 },
+      uhr: { m: [0] },
+    },
+  ],
+  ru: [
+    {
+      сейчас: { h: true },
+      четверть: { m: [15, 45] },
+      половина: { m: [30] },
+    },
+    {
+      пять: { m: [5, 25, 35, 55] },
+      десять: { m: [10, 50] },
+      двадцать: { m: [20, 40] },
+    },
+    {
+      до: { m: [45, 50, 55] },
+      после: { m: [5, 10, 15, 20, 25] },
+      один: { h: 1, next_h_from_minute: 30 },
+      два: { h: 2, next_h_from_minute: 30 },
+    },
+    {
+      три: { h: 3, next_h_from_minute: 30 },
+      четыре: { h: 4, next_h_from_minute: 30 },
+      пять: { h: 5, next_h_from_minute: 30 },
+    },
+    {
+      шесть: { h: 6, next_h_from_minute: 30 },
+      семь: { h: 7, next_h_from_minute: 30 },
+      восемь: { h: 8, next_h_from_minute: 30 },
+    },
+    {
+      девять: { h: 9, next_h_from_minute: 30 },
+      десять: { h: 10, next_h_from_minute: 30 },
+      одиннадцать: { h: 11, next_h_from_minute: 30 },
+    },
+    {
+      двенадцать: { h: 0, next_h_from_minute: 30 },
+      часов: { m: [0] },
     },
   ],
 };
