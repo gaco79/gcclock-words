c4fdb52c9a0a61e8500c9507840cee22d8655b45
Gareth Cooper <gareth@garethcooper.com>
2025-04-01 19:07:13 +0100

    refactor: remove redundant next_h_from_minute from clock definitions

--- src/types/ClockDefinition.ts
+++ src/types/ClockDefinition.ts
@@ -2,8 +2,6 @@
 interface WordCondition {
   /** Hour condition */
   h?: number[];
-  /** Minute threshold to start using next hour */
-  next_h_from_minute?: number;
   /** Array of minute values (multiples of 5) */
   m?: number[];
 }
@@ -17,4 +15,6 @@ interface ClockLine {
 export type ClockDefinition = {
   styles?: string;
   lines: ClockLine[];
+  /** Minute threshold to start using next hour */
+  next_h_from_minute: number;
 };
